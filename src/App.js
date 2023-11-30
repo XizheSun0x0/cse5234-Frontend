0df6159e566e28cdf988b283e598aa633019f3eb
Ben He <he.1658@osu.edu>
2023-11-29 19:25:09 -0500

    fix: fix some style on the webpage

--- src/App.js
+++ src/App.js
@@ -1,66 +1,72 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/react-in-jsx-scope */
-import './App.css';
-import React,{useState, useEffect} from "react";// for connection with backend
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
-import Purchase from './components/purchase';
-import PaymentEntry from './components/paymentEntry';
-import ShippingEntry from './components/shippingEntry';
-import ViewOrder from './components/viewOrder';
-import ViewConfirmation from './components/viewConfirmation';
+import "./App.css";
+import React, { useState, useEffect } from "react"; // for connection with backend
+import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Purchase from "./components/purchase";
+import PaymentEntry from "./components/paymentEntry";
+import ShippingEntry from "./components/shippingEntry";
+import ViewOrder from "./components/viewOrder";
+import ViewConfirmation from "./components/viewConfirmation";
 
 import SampleFooter from "./components/footer";
-import Home from './components/home';
-import About from './components/about';
-import Cart from './components/cart';
-import Support from './components/support';
-
+import Home from "./components/home";
+import About from "./components/about";
+import Cart from "./components/cart";
+import Support from "./components/support";
+import background from "./components/images/rm347-porpla-02-a-01.jpg";
 
 function App() {
-
   const [data, setData] = useState([{}]);
 
-    useEffect(() => {
-        // Fetch the data from the Flask backend
-        fetch("/home").then(
-          res => res.json()
-        ).then(
-          data => {
-            setData(data);
-            console.log(data);
-          }
-        )
-    }, []);
+  useEffect(() => {
+    // Fetch the data from the Flask backend
+    fetch("/home")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
 
+  const myStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "repeat",
+  };
 
   return (
-    <div className="App">
-    <Router>
-    <Navbar />
-      <div className="content">
-      <Routes>  
-      <Route path='/home' element={<Home/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/support' element={<Support/>}/>
-      <Route path='/cart' element={<Cart/>} />
+    <div className="App" style={myStyle}>
+      <Router>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/support" element={<Support />} />
+            <Route path="/cart" element={<Cart />} />
 
-      <Route path='/purchase' element={<Purchase/>} />
-      <Route path="/" element={<Navigate replace to="/home" />} />
-      <Route path='/purchase/paymentEntry' element={<PaymentEntry/>} />
-      <Route path='/purchase/shippingEntry' element={<ShippingEntry/>} />
-      <Route path='/purchase/viewOrder' element={<ViewOrder/>} />
-      <Route path='/purchase/viewConfirmation' element={<ViewConfirmation/>} />
-      </Routes>
-      </div>
-      <SampleFooter />
-    </Router>
+            <Route path="/purchase" element={<Purchase />} />
+            <Route path="/" element={<Navigate replace to="/home" />} />
+            <Route path="/purchase/paymentEntry" element={<PaymentEntry />} />
+            <Route path="/purchase/shippingEntry" element={<ShippingEntry />} />
+            <Route path="/purchase/viewOrder" element={<ViewOrder />} />
+            <Route
+              path="/purchase/viewConfirmation"
+              element={<ViewConfirmation />}
+            />
+          </Routes>
+        </div>
+        <SampleFooter />
+      </Router>
     </div>
-
-
   );
-  
 }
 
 export default App;
-
--- src/components/cart.js
+++ src/components/cart.js
@@ -1,82 +1,96 @@
-import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import "./cart.css";
-import emptyCart from './images/empty-cart.png';
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import './cart.css'
+import emptyCart from './images/empty-cart.png'
 
 const Cart = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const handlesubmit = () => {
-    navigate("/paymentEntry", { state: location.state });
-  };
-  const handleBackToPurchase = () => {
-    navigate("/purchase");
-  }
-  console.log(location.state);
-
-  const RenderEmptyCart = () => {
-    return (
-      <div className="empty-cart-container">
-        <div className="empty-cart">
-          <img src={emptyCart} title="empty cart icons" width="100" height="100" className="empty-cart-img"/>
-        </div>
-        <h2>I&apos;M EMPTY :-( PLEASE ADD PRODUCTS.</h2>
-        <button id="back-to-purchase" type="submit" className="button" onClick={handleBackToPurchase}>
-          Continue Shopping
-        </button>
-      </div>
-    );
-  };
-
-  const RenderCart = () => {
-    if (location.state !== null) {
-      const itemRows = Array.from(location.state.selected_items).map(
-        ([item, quantity]) => (
-          <tr key={item.name}>
-            <td>{item.name}</td>
-            <td>${item.price}</td>
-            <td>{quantity}</td>
-          </tr>
-        )
-      );
-  
-      // Calculate total price
-      const totalPrice = Array.from(location.state.selected_items).reduce(
-        (total, [item, quantity]) => total + item.price * quantity,
-        0
-      );
-  
-      // Return the table of JSX elements and total price
-      return (
-        <form onSubmit={handlesubmit}>
-          <table className="cart-table">
-            <thead>
-              <tr>
-                <th>Item</th>
-                <th>Price</th>
-                <th>Quantity</th>
-              </tr>
-            </thead>
-            <tbody>{itemRows}</tbody>
-          </table>
-          <div className="total-price">Total Price: ${totalPrice.toFixed(2)}</div>
-          <button type="submit" className="button">
-          Go to Payment
-        </button>
-      </form>
-      );
-    } else {
-      return <><RenderEmptyCart /></>;
+    const location = useLocation()
+    const navigate = useNavigate()
+    const handlesubmit = () => {
+        navigate('/paymentEntry', { state: location.state })
     }
-  };
-  
-  
-  return (
-    <div className="cart">
-      <h1>Shopping Cart</h1>
-        <RenderCart />
-    </div>
-  );
-};
-  
-export default Cart;
+    const handleBackToPurchase = () => {
+        navigate('/purchase')
+    }
+    console.log(location.state)
+
+    const RenderEmptyCart = () => {
+        return (
+            <div className="empty-cart-container">
+                <div className="empty-cart">
+                    <img
+                        src={emptyCart}
+                        title="empty cart icons"
+                        width="150"
+                        height="150"
+                        className="empty-cart-img"
+                    />
+                </div>
+                <h2>I&apos;M EMPTY :-( PLEASE ADD PRODUCTS.</h2>
+                <button
+                    id="back-to-purchase"
+                    type="submit"
+                    className="button"
+                    onClick={handleBackToPurchase}
+                >
+                    Continue Shopping
+                </button>
+            </div>
+        )
+    }
+
+    const RenderCart = () => {
+        if (location.state !== null) {
+            const itemRows = Array.from(location.state.selected_items).map(
+                ([item, quantity]) => (
+                    <tr key={item.name}>
+                        <td>{item.name}</td>
+                        <td>${item.price}</td>
+                        <td>{quantity}</td>
+                    </tr>
+                )
+            )
+
+            // Calculate total price
+            const totalPrice = Array.from(location.state.selected_items).reduce(
+                (total, [item, quantity]) => total + item.price * quantity,
+                0
+            )
+
+            // Return the table of JSX elements and total price
+            return (
+                <form onSubmit={handlesubmit}>
+                    <table className="cart-table">
+                        <thead>
+                            <tr>
+                                <th>Item</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                            </tr>
+                        </thead>
+                        <tbody>{itemRows}</tbody>
+                    </table>
+                    <div className="total-price">
+                        Total Price: ${totalPrice.toFixed(2)}
+                    </div>
+                    <button type="submit" className="button">
+                        Go to Payment
+                    </button>
+                </form>
+            )
+        } else {
+            return <RenderEmptyCart />
+        }
+    }
+
+    return (
+        <div className="cart">
+            <div>
+                <h1>Shopping Cart</h1>
+            </div>
+            <RenderCart />
+        </div>
+    )
+}
+
+export default Cart
